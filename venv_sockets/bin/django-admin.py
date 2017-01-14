81f41ef491216364d2def6f46b91989105647331
David Axelrod <daxaxelrod@gmail.com>
2017-01-13 23:32:31 -0500

    initial commit. learning about websockets and channels

---
+++ venv_sockets/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/davidaxelrod/Documents/playing_around/sockets/venv_sockets/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
